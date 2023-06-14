23530a5eadb0f2ba37c84a12800c6702b872c4d0
LuxScintilla <catherina.ea@gmail.com>
2023-06-14 11:19:59 +0100

    Start to create folder functionality

--- src/index.js
+++ src/index.js
@@ -1,47 +1,69 @@
 import _ from "lodash";
 import "./style.css";
-import loadTaskPage from "./tasks.js";
-import loadTodayPage from "./today.js";
-import loadCompletedPage from "./completed.js";
-import loadNewPage from "./new-task";
 
-const mainWrapper = document.querySelector(".main-wrapper");
-const tasksButton = document.querySelector(".tasks-button");
-const todayButton = document.querySelector(".today-button");
-const completedButton = document.querySelector(".completed-button");
-const newButton = document.querySelector(".new-button");
+const folderList = document.querySelector(".folder-list");
+const newFolderForm = document.querySelector("#new-folder-form");
+const newFolderInput = document.querySelector(".new-folder-input");
+const newFolderSubmit = document.querySelector(".new-folder-submit");
 
-(function loadFirst() {
-  while (mainWrapper.firstChild) {
-    mainWrapper.removeChild(mainWrapper.firstChild);
+const LOCAL_STORAGE_FOLDERS_KEY = "task.folders";
+const LOCAL_STORAGE_SELECTED_FOLDER_ID_KEY = "task.selectedFolderId";
+let allFolders =
+  JSON.parse(localStorage.getItem(LOCAL_STORAGE_FOLDERS_KEY)) || [];
+let selectedFolderId = localStorage.getItem(
+  LOCAL_STORAGE_SELECTED_FOLDER_ID_KEY
+);
+
+folderList.addEventListener("click", (e) => {
+  if (e.target.tagName.toLowerCase() === "li") {
+    selectedFolderId = e.target.dataset.listID;
+    saveAndRender();
   }
-  loadTaskPage();
-})();
-
-tasksButton.addEventListener("click", function () {
-  while (mainWrapper.firstChild) {
-    mainWrapper.removeChild(mainWrapper.firstChild);
-  }
-  loadTaskPage();
 });
 
-todayButton.addEventListener("click", function () {
-  while (mainWrapper.firstChild) {
-    mainWrapper.removeChild(mainWrapper.firstChild);
-  }
-  loadTodayPage();
+newFolderForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const folderName = newFolderInput.value;
+  if (folderName == null || folderName === "") return;
+  const folder = createFolder(folderName);
+  newFolderInput.value = "";
+  allFolders.push(folder);
+  saveAndRender();
+  console.log(allFolders);
 });
 
-completedButton.addEventListener("click", function () {
-  while (mainWrapper.firstChild) {
-    mainWrapper.removeChild(mainWrapper.firstChild);
-  }
-  loadCompletedPage();
-});
+function createFolder(name) {
+  return { id: Date.now().toString(), name: name, tasks: [] };
+}
 
-newButton.addEventListener("click", function () {
-  while (mainWrapper.firstChild) {
-    mainWrapper.removeChild(mainWrapper.firstChild);
+function saveAndRender() {
+  save();
+  render();
+}
+
+function save() {
+  localStorage.setItem(LOCAL_STORAGE_FOLDERS_KEY, JSON.stringify(allFolders));
+  localStorage.setItem(LOCAL_STORAGE_SELECTED_FOLDER_ID_KEY, selectedFolderId);
+}
+
+function render() {
+  clearElement(folderList);
+  allFolders.forEach((folder) => {
+    const listElement = document.createElement("li");
+    listElement.classList.add("folder");
+    listElement.dataset.listID = folder.id;
+    listElement.textContent = folder.name;
+    if (folder.id === selectedFolderId) {
+      listElement.classList.add("active-folder");
+    }
+    folderList.appendChild(listElement);
+  });
+}
+
+function clearElement(element) {
+  while (element.firstChild) {
+    element.removeChild(element.firstChild);
   }
-  loadNewPage();
-});
+}
+
+render();
