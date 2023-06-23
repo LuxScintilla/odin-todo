94df4e046dbb0075d4ab33b2f01e7107f6870bb5
LuxScintilla <catherina.ea@gmail.com>
2023-06-23 20:42:52 +0100

    Add edit task functionality

--- src/create-dom.js
+++ src/create-dom.js
@@ -242,6 +242,7 @@ export const createDOM = {
 
           const taskEdit = document.createElement("div");
           taskEdit.classList.add("task-edit");
+          taskEdit.addEventListener("click", createDOM.editTaskElement);
 
           const taskDelete = document.createElement("div");
           taskDelete.classList.add("task-delete");
@@ -254,5 +255,69 @@ export const createDOM = {
         });
       }
     }
+  },
+
+  editTaskElement() {
+    const ID = this.parentElement.dataset.taskID;
+
+    const modalDialog = document.createElement("dialog");
+    modalDialog.classList.add("modal");
+
+    const modalTitle = document.createElement("h2");
+    modalTitle.classList.add("modal-title");
+    modalTitle.textContent = "Insert your new task name";
+
+    const modalForm = document.createElement("form");
+    modalForm.classList.add("modal-form");
+    modalForm.setAttribute("method", "dialog");
+
+    const modalInput = document.createElement("input");
+    modalInput.classList.add("modal-input");
+    modalInput.setAttribute("type", "text");
+    modalInput.setAttribute("name", "folder-edit");
+    modalInput.setAttribute("autocomplete", "off");
+    modalInput.setAttribute("required", "");
+
+    const buttonDIV = document.createElement("div");
+    buttonDIV.classList.add("button-div");
+
+    const modalSubmit = document.createElement("button");
+    modalSubmit.classList.add("modal-submit");
+    modalSubmit.setAttribute("type", "button");
+    modalSubmit.textContent = "Submit";
+    modalSubmit.addEventListener("click", function (e) {
+      e.preventDefault();
+      if (modalInput.value === null || modalInput.value === "") {
+        return;
+      } else {
+        FOLDERS.getStorage();
+        FOLDERS.names.forEach((folder) => {
+          folder.tasks.forEach((task) => {
+            if (task.id === +ID) {
+              task.name = modalInput.value;
+              FOLDERS.saveObject();
+              render();
+              createDOM.createTaskElement();
+              modalDialog.close();
+            }
+          });
+        });
+      }
+    });
+
+    const modalCancel = document.createElement("button");
+    modalCancel.classList.add("modal-cancel");
+    modalCancel.setAttribute("type", "button");
+    modalCancel.textContent = "Cancel";
+    modalCancel.addEventListener("click", function () {
+      modalDialog.close();
+    });
+
+    buttonDIV.append(modalSubmit, modalCancel);
+    modalForm.append(modalInput, buttonDIV);
+    modalDialog.append(modalTitle, modalForm);
+    folderList.appendChild(modalDialog);
+
+    modalDialog.showModal();
   },
 };
